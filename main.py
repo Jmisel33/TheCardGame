23413bf630dfb1f688903f6f54c4b57a2938bbeb
Jmisel33 <jmiselnicky@sjnma.org>
2017-03-06 17:51:54 -0600

    progress1

--- main.py
+++ main.py
@@ -1,68 +1,17 @@
 import random
 from card import Card
+from deck import Deck
 
-class Deck:
-    def __init__(self):
-		self.value =[]
-		self.validSuits=["Clubs","Spades","Hearts","Diamonds"]
-		self.validRanks=["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
-		for suit in self.validSuits:
-			for rank in self.validRanks:
-			    self.value.append(Card(rank,suit))
-    def DisplayDeck(self):
-        for c in self.value:
-    	    c.displayCard()
-    def Shuffle(self):
-    	firstHalf,secondHalf = self.Cut()
-    	self.Combine(firstHalf,secondHalf)
-    	
-    def TakeFromTop(self):
-    	c = self.value[-1]
-    	self.value.remove(c)
-    	return c
-    def TakeFromBottom(self):
-    	#bottom is 0, top is 52
-    	c = self.value[0]
-    	self.value.remove(c)
-    	return c
+print("The Game is Black Jack. The Rules are simple, Its you vs. the dealer. You will be delt two cards and if you would like to get another type hit or if you'd like to stay type stay. Got it? ")
 
-    def AddToBottom(self,cardToAdd):
-    	self.value.insert(0,cardToAdd)
-    def AddToTop(self,cardToAdd):
-    	self.value.append(cardToAdd)
+raw_input("Press enter to start the game")
 
-    def Cut(self):
-    	first = Deck()
-    	second = Deck()
-    	halfway = len(self.value)/2
-    	first.value = self.value[halfway:]
-    	second.value=self.value[:halfway]
-    	self.value = []
-    	return first,second
-
-    def Combine(self,first,second):
-        ticker=True
-        while(first.value != [] or second.value !=[]):
-            cardToAdd = Card("temp","temp")
-            if(ticker and first != []):
-                cardToAdd=first.TakeFromTop()
-            elif(not ticker and second != []):
-                cardToAdd=second.TakeFromTop()
-            self.value.insert(random.randint(0,len(self.value)),cardToAdd)
-            ticker = not ticker
+dealershand = Deck()
+dealershand. Shuffle()
+currentcard = dealershand. TakeFromTop()
 
 
 
-    def Concat(self,first,second):
-    	self.value = first.value + second.value
-
-if(__name__=="__main__"):
-    startingDeck = Deck()
-    print(len(startingDeck.value))
-    startingDeck.DisplayDeck()
-    startingDeck.Shuffle()
-    print("00000000000000000000000000000000")
-    startingDeck.DisplayDeck()
-
-
-print len(startingDeck.value)
+p1hand = Deck()
+p1hand. Shuffle()
+p1hand. TakeFromTop()
