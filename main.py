b03aa0499a3382c62ab8bbb9ec4d2de881f08011
Jmisel33 <jmiselnicky@sjnma.org>
2017-03-06 20:29:30 -0600

    got the game to work, need to add more details

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 from card import Card
 from deck import Deck
 
+score = 0
 
 
 
@@ -12,28 +13,36 @@ print("The Game is Black Jack. The Rules are simple, Its you vs. the dealer. You
 raw_input("Press enter to start the game")
 
 print("Dealers hand")
+raw_input("Press enter for the deal")
 dealershand = Deck()
 dealershand. Shuffle()
 currentcard = dealershand. TakeFromTop()
 currentcard. displayCard()
 currentcard = dealershand. TakeFromTop()
 currentcard. displayCard()
-raw_input("Would you like to hit for another card?")
-if ("yes"):
-    currentcard = dealershand. TakeFromTop()
-    currentcard. displayCard()
 
 
 
+
+raw_input("Press enter for the deal")
 print("Player Ones Deal")
+
+
 p1hand = Deck()
 p1hand. Shuffle()
 currentcard = p1hand. TakeFromTop()
+score = currentcard.orderRank + score
 currentcard. displayCard()
 currentcard = p1hand. TakeFromTop()
+score = currentcard.orderRank + score
 currentcard. displayCard()
-
-raw_input("Would you like to hit for another card?")
-if ("yes"):
-    currentcard = dealershand. TakeFromTop()
+extracard = raw_input("Would you like to hit for another card?")
+if extracard == "yes":
+    currentcard = p1hand. TakeFromTop()
+    score = currentcard.orderRank + score
     currentcard. displayCard()
+
+print(score)
+if score > 21:
+    print("BUST")
+
